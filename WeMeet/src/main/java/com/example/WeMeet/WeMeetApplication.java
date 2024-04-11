2d244bbc46a578aa5a347f5e876028c0c8b8a366
GwanSuNi <tony2yoo@gmail.com>
2024-04-11 10:31:02 +0900

    Add: 프로젝트 설정 및 의존성 초기화

---
+++ WeMeet/src/main/java/com/example/WeMeet/WeMeetApplication.java
@@ -0,0 +1,13 @@
+package com.example.WeMeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeMeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeMeetApplication.class, args);
+	}
+
+}
