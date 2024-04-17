14760079447596d3ef7e5a482a2400f760c42da4
sibjagun <sibjagun99@kyungmin.ac.kr>
2024-04-17 18:07:46 +0900

    Add: "메인화면 반응형 추가"

--- WeMeet/src/main/wemeet_front/src/App.tsx
+++ WeMeet/src/main/wemeet_front/src/App.tsx
@@ -6,16 +6,50 @@ import Footer from "./components/Footer";
 import StickyFooter from "./components/StickyFooter";
 import Box from "@mui/material/Box";
 import MainLayout from "./layout/MainLayout";
+import { useMediaQuery } from 'react-responsive'
 
 function App() {
 
     return (
         <>
+            <Desktop/>
+            <Tablet/>
+            <Mobile/>
+        </>
+    );
+}
+const Desktop = () => {
+    const isDesktop = useMediaQuery({ minWidth: 1025 })
+    return isDesktop ? (
+        <div className='desktop-frame'>
+            <div className='smop'>SMOP</div>
+            <div className='desktop'>
+                <MainLayout>
+                    <SignUp/>
+                </MainLayout>
+            </div>
+        </div>
+    ) : null
+}
+const Tablet = () => {
+    const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 })
+    return isTablet ? (
+        <div className='tablet'>
             <MainLayout>
                 <SignUp/>
             </MainLayout>
-        </>
-    );
+        </div>
+    ) : null
+}
+const Mobile = () => {
+    const isMobile = useMediaQuery({ maxWidth: 767 })
+    return isMobile ? (
+        <div className='mobile'>
+            <MainLayout>
+                <SignUp/>
+            </MainLayout>
+        </div>
+    ) : null
 }
 
 export default App;
