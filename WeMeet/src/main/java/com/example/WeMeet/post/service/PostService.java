7f76137acb78d379032bb3ecceecfbbc406ecc70
sibjagun <sibjagun99@kyungmin.ac.kr>
2024-06-11 21:17:50 +0900

    Add: "PostService추가"

--- WeMeet/src/main/java/com/example/WeMeet/post/service/PostService.java
+++ WeMeet/src/main/java/com/example/WeMeet/post/service/PostService.java
@@ -1,10 +1,15 @@
 package com.example.WeMeet.post.service;
 
+import com.example.WeMeet.post.domain.dto.CreatePostDto;
 import com.example.WeMeet.post.domain.dto.PostDto;
+import com.example.WeMeet.post.domain.dto.UpdatePostDto;
+
+import java.util.List;
+import java.util.UUID;
 
 public interface PostService {
-    public PostDto savePost(PostDto postDto);
-    public PostDto updatePost(PostDto postDto);
-    public PostDto getPost(Long id);
-    public void deletePost(Long id);
+    public UUID savePost(CreatePostDto createPostDto);
+    public void updatePost(UUID postUUID, UpdatePostDto updatePostDto);
+    public PostDto getPost(UUID postUUID);
+    public void deletePost(UUID postUUID);
 }
