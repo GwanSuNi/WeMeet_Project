8615e1939915f78249770966255008b382d0984e
sibjagun <sibjagun99@kyungmin.ac.kr>
2024-06-12 16:32:35 +0900

    Improve: "PostPhoto 상대경로로 저장으로 수정"

--- WeMeet/src/main/java/com/example/WeMeet/post/service/PostPhotoServiceImpl.java
+++ WeMeet/src/main/java/com/example/WeMeet/post/service/PostPhotoServiceImpl.java
@@ -1,18 +1,15 @@
 package com.example.WeMeet.post.service;
 
 import com.example.WeMeet.global.exception.ResourceNotFoundException;
-import com.example.WeMeet.global.exception.ValidationException;
 import com.example.WeMeet.post.domain.entity.PostPhoto;
 import com.example.WeMeet.post.repository.PostPhotoRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.NotDirectoryException;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
@@ -28,22 +25,19 @@ import java.util.stream.Stream;
 public class PostPhotoServiceImpl implements PostPhotoService {
     private final PostPhotoRepository postPhotoRepository;
     private static final String RELATIVE_PATH_POST = "/db/weMeet/post/";
-    private static final String RELATIVE_PATH_PROFILE = "/db/weMeet/profile/";
     private static final String UPLOAD_DIR_POST = System.getProperty("user.home") + RELATIVE_PATH_POST;
-    private static final String UPLOAD_DIR_PROFILE = System.getProperty("user.home") + RELATIVE_PATH_PROFILE;
 
     /**
-     *
      * @param uploadPostPath
      * @param postUUID
      * @param index
      * @return
      */
-    private Path getFilePath(String uploadPostPath, UUID postUUID, int index) {
+    private Path getPostFilePath(String uploadPostPath, UUID postUUID, int index) {
         return Paths.get(uploadPostPath).resolve(postUUID + "_" + index + ".png");
     }
+
     /**
-     *
      * @param userEmail
      * @param postUUID
      * @param files
@@ -59,32 +53,33 @@ public class PostPhotoServiceImpl implements PostPhotoService {
             Files.createDirectories(uploadPath);
         }
         for (int i = 0; i < files.length; i++) {
-            savePostImage(uploadPostPath, postUUID, files[i], i);
+            savePostImage(userEmail,uploadPostPath, postUUID, files[i], i);
         }
     }
 
     /**
-     *
      * @param uploadPostPath
      * @param postUUID
      * @param file
      * @param index
-     * @return
      * @throws IOException
      */
     @Override
-    public PostPhoto savePostImage(String uploadPostPath, UUID postUUID, MultipartFile file, int index) throws IOException {
+    public void savePostImage(String userEmail, String uploadPostPath, UUID postUUID, MultipartFile file, int index) throws IOException {
         // 사진 이름 정하기
-        Path filePath = getFilePath(uploadPostPath, postUUID, index);
+        Path filePath = getPostFilePath(uploadPostPath, postUUID, index);
         Files.copy(file.getInputStream(), filePath);
+        // DB에 저장할때 상대경로로 저장
+        String uploadPostPathDB = RELATIVE_PATH_POST + userEmail + "/" + postUUID;
         PostPhoto postPhoto = new PostPhoto();
         postPhoto.setPostUUID(postUUID);
-        postPhoto.setImgPath(filePath.toString());
+        postPhoto.setImgPath(uploadPostPathDB);
         postPhotoRepository.save(postPhoto);
-        return postPhoto;
     }
 
-    /** 게시물의 이미지 삭제하기 메서드
+    /**
+     * 게시물의 이미지 삭제하기 메서드
+     *
      * @param postUUID
      * @param userEmail
      */
@@ -108,7 +103,10 @@ public class PostPhotoServiceImpl implements PostPhotoService {
         deletePostImagePath(postUUID);
         // postUUID를 갖고있는경로의 데이터 찾아서 삭제하기
     }
-    
+
+    /**
+     * @param postUUID
+     */
     @Override
     public void deletePostImagePath(UUID postUUID) {
         List<PostPhoto> postPhotoList = postPhotoRepository.findByPhotoUUID(postUUID);
@@ -116,19 +114,18 @@ public class PostPhotoServiceImpl implements PostPhotoService {
     }
 
     /**
-     *
      * @param postUUID
      * @param userEmail
      * @return
      * @throws IOException
      */
     @Override
-    public List<byte[]>getPostImages(UUID postUUID,String userEmail) throws IOException {
+    public List<byte[]> getPostImages(UUID postUUID, String userEmail) throws IOException {
         String uploadPostPath = UPLOAD_DIR_POST + userEmail + "/" + postUUID;
         int index = postPhotoRepository.countByPostUUID(postUUID);
         List<byte[]> images = new ArrayList<>();
         for (int i = 0; i < index; i++) {
-            Path filePath = getFilePath(uploadPostPath, postUUID, i);
+            Path filePath = getPostFilePath(uploadPostPath, postUUID, i);
             images.add(Files.readAllBytes(filePath));
         }
         return images;
